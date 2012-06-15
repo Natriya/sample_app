86750aca65c240899aa305f27d80e9331adc94b6
Fafa <fafadjignofame@yahoo.fr>
2012-06-15 15:38:14 +0200

    Fin chap 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+   # Retourne un titre basé sur la page.
+   def titre
+       base_titre = "Simple App du Tutoriel Ruby on Rails"
+	   if @titre.nil?
+	      base_titre
+	   else 
+	      "#{base_titre} | #{@titre}"
+	   end
+   end
 end
