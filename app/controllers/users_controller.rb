e0e097d83c23fb1ae35b26afd9c8872141d14478
Fafa <fafadjignofame@yahoo.fr>
2012-06-17 15:05:48 +0200

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   	@titre = "Inscription"
   end
 
   def show
     @user = User.find(params[:id])
     @titre = @user.nom
+  end
+
+  def create
+    @user= User.new(params[:user])
+    if @user.save
+        flash[:success] = "Bienvenue dans l'Application Exemple!"
+        redirect_to @user
+    else
+        @titre = "Inscription"
+        render 'new'
+    end
   end
 end
